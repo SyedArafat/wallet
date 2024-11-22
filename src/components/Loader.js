27f27df61452d7780566bf8f06f427e1cfe20ef1
Sayed Yeamin Arafat <syedyeaminarafat@gmail.com>
2024-11-22 14:27:13 +0600

    Experimental loader added

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Loader() {
+    return (
+        <div className="section-b-space">
+            <div className="custom-container">
+                <div className="loader text-center">
+                    <img className="img-fluid loader-img" src="assets/images/gif/pay-loader.gif" alt="loader" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Loader;
